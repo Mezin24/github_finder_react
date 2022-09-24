914dfecc0d05c7d084d28d304d1c0f6f824f9401
Pavel Mezencev <mezencev24@mail.ru>
2022-09-24 21:41:36 +0300

    add chars

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -6,34 +6,60 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = useGithubContext();
   console.log(repos);
-  let data = repos.reduce((acc, cur) => {
-    if (acc[cur.language]) {
-      acc[cur.language] = acc[cur.language] + 1;
+
+  const languages = repos.reduce((total, repo) => {
+    const { language, stargazers_count: stars } = repo;
+    if (!language) return total;
+    if (total[language]) {
+      total[language].value = total[language].value + 1;
+      total[language].stars = total[language].stars + stars;
     } else {
-      acc[cur.language] = 1;
+      total[language] = { label: language, value: 1, stars };
     }
-    return acc;
+
+    return total;
   }, {});
 
-  let chartData = [];
+  const mostPopulatLngs = Object.values(languages)
+    .sort((a, b) => b - a)
+    .map((item) => ({ label: item.label, value: item.value }))
+    .slice(0, 5);
 
-  for (let [key, value] of Object.entries(data)) {
-    if (key === 'null') continue;
-    chartData.push({
-      label: key,
-      value,
-    });
-  }
+  const starsPerLngs = Object.values(languages)
+    .sort((a, b) => b - a)
+    .map((item) => ({ label: item.label, value: item.stars }))
+    .slice(0, 5);
 
-  chartData = chartData.sort((a, b) => b.value - a.value).slice(0, 5);
+  let { stars, forks } = repos.reduce(
+    (total, language) => {
+      const { stargazers_count, forks, name } = language;
+
+      total.stars[stargazers_count] = {
+        label: name,
+        value: stargazers_count,
+      };
+      total.forks[forks] = { label: name, value: forks };
+
+      return total;
+    },
+    { stars: {}, forks: {} }
+  );
+
+  stars = Object.values(stars)
+    .slice(-5)
+    .sort((a, b) => b.value - a.value);
+
+  forks = Object.values(forks)
+    .slice(-5)
+    .sort((a, b) => b.value - a.value);
 
   return (
     <section className='section'>
       <Wrapper className='section-center'>
-        <Pie3D data={chartData} />
-        <div></div>
-        <Doughnut2D data={chartData} />
-        <div></div>
+        <Pie3D data={mostPopulatLngs} />
+        <Column3D data={stars} />
+        <Doughnut2D data={starsPerLngs} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
